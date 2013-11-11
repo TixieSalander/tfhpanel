2940e4251f59e41529f687d75142e0727cd0a7b7
0xa <xa@xomg.net>
2013-11-11 02:46:32 +0100

    Improve bool/foreignkey list format

--- tfhpanel/tplutils.py
+++ tfhpanel/tplutils.py
@@ -15,4 +15,20 @@ def get_root_panels_dict():
     #TODO: remove.
     return RootFactory.children
 
+def format_panel_value(value):
+    if isinstance(value, bool):
+        # &#x2714; HEAVY CHECK MARK / &#x2718; HEAVY BALLOT X
+        # &#x25cf; BLACK CIRCLE / &#x25cb; WHITE CIRCLE
+        boolvalue = '&#x2714;' if value else '&#x2718;'
+        return '<span class="panel-value-bool">'+boolvalue+'</span>'
+    return '<span class="panel-value">'+str(value)+'</span>'
+
+def find_view(dbo, root=RootFactory.children):
+    for v in root.values():
+        if isinstance(dbo, v.model):
+            return v
+        elif v.children:
+            r = find_view(dbo, root.children)
+            if r:
+                return r
 
